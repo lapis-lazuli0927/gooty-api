9ccc1ad7582534743d83690d33282df46f7d2c1c
lapis-lazuli0927 <r09270441@gmail.com>
2025-11-21 16:36:11 +0900

    feat: StationとShopモデルを追加
    - StationとShopのマイグレーションファイルを作成
    - モデルファイルとテストファイルを追加

---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.0].define(version: 2025_11_21_060749) do
+  create_table "shops", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", limit: 30, null: false
+    t.text "url"
+    t.bigint "station_id"
+    t.string "adress", limit: 30
+    t.integer "tel"
+    t.text "memo"
+    t.integer "review"
+    t.boolean "is_ai_generated", null: false
+    t.boolean "is_instagram", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["station_id"], name: "index_shops_on_station_id"
+  end
+
+  create_table "stations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", limit: 30, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "shops", "stations"
+end
