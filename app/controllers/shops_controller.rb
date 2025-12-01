1b40966ee9c0d65a041991975a135bb63ca13288
lapis-lazuli0927 <r09270441@gmail.com>
2025-12-01 17:02:19 +0900

    feat: controllerの作成とルーティング追加

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,5 @@
+class ShopsController < ApplicationController
+  def index
+    render json: { data: "ruri" }
+  end
+end
---
+++ spec/requests/shops_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Shops", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/shops/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
